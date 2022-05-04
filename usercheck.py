afe4699b072601540f6592bf0d99f541378f9cbf
nadimshrestha <nadimshrestha@gmail.com>
2022-05-04 18:00:20 +0545

    user input, operator

---
+++ usercheck.py
@@ -0,0 +1,8 @@
+#username and password checking
+username = input("Enter the username:")
+password = input("Enter the password:")
+
+if(username == "admin" and password == "P@ssw0rd"):
+    print("Credential Match")
+else:
+    print("Invalid Username or Password")
---
+++ username.py
@@ -0,0 +1,14 @@
+#userInput
+num1 = int(input("Enter the first Number:"))
+num2 = int(input("Enter the Second Number:"))
+num3 = int(input("Enter the Third Number:"))
+if(num1>=num2 and num1>=num3) :
+    print("{0} is greater than {1} and {2}".format(num1, num2, num3))
+elif(num2>=num3 and num2>=num1):
+    print("{0} is greater than {1} and {2}".format(num2, num1, num3))
+else:
+    print("{0} is greater than {1} and {2}".format(num3, num2, num1))
+
+
+
+
